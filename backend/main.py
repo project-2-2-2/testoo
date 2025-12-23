1e78829c22e4fc7b8d8d2afb244e9a7acac939d6
project-2-2-2 <sughanvasan14@gmail.com>
2025-12-23 13:44:22 +0530

    final check

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:5174",
-        "http://127.0.0.1:5174",
+        "https://testoo-tau.vercel.app/",
     ],
     allow_credentials=False,
     allow_methods=["*"],
